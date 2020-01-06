46e8dfb76b8d8a2327aee7ac07c7b31101299430
zrinGithub <806488823@qq.com>
2020-01-06 19:50:43 +0800

    [init]

---
+++ src/test/java/com/zr/shirodemo/QuickStart.java
@@ -0,0 +1,18 @@
+package com.zr.shirodemo;
+
+import org.junit.Before;
+
+/**
+ * Description:
+ * 简单测试 shiro 认证流程
+ * @author zhangr
+ * 2020/1/6 19:48
+ */
+public class QuickStart {
+
+
+    @Before
+    public void init(){
+
+    }
+}
